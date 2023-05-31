c74c048f422f5cc4fa756e212dda43849fd99047
Aleksandr Antashkevich <antoha_ale6@mail.ru>
2023-05-31 17:42:50 +0200

    create header, footer

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={`container ${styles.footerContainer}`}>
+        <div>
+          <h3 className={styles.blockTitle}>Categories</h3>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <a className={styles.link} href="">Flowers</a>
+            </li>
+            <li className={styles.listItem}>
+              <a className={styles.link} href="">Gifts </a>
+            </li>
+            <li className={styles.listItem}>
+              <a className={styles.link} href="">Roses </a>
+            </li>
+            <li className={styles.listItem}>
+              <a className={styles.link} href=""> Occasion</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className={styles.blockTitle}>Information</h3>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <a className={styles.link} href="">Shop</a>
+            </li>
+            <li className={styles.listItem}>
+              <a className={styles.link} href="">About us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
